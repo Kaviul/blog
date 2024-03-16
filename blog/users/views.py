25ba2a5b40a572c2423b76f06649d4b36da14e23
MD KAVIUL HOSSAIN <kaviuln@gmail.com>
2024-03-17 05:54:40 +1100

    new update

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import UserCreationForm
 
 # Create your views here.
 
 class RegisterView(View):
     def get(self, request):
-        pass
+        form = UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form})
+        
+        
     def post(self, request):
         pass
