07029372bfd47e92f62ad4814b6562b61195a1ed
MD KAVIUL HOSSAIN <kaviuln@gmail.com>
2024-03-19 04:32:30 +1100

    updates

--- blog/users/views.py
+++ blog/users/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from django.template import RequestContext
 from django.views import View
 from .forms import UserRegisterForm
-#from django.http import HttpResponse
-#from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
@@ -11,7 +11,7 @@ class RegisterView(View):
         form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form})
         
- #   @csrf_exempt   
+ 
     def post(self, request):
         form = UserRegisterForm(request.POST)
         
